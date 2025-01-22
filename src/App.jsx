523d416a3b4ce624898924517f15148b31802df1
Lylyneru <lyly777@gmail.com>
2025-01-22 13:43:06 +0200

    add new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-// import React, { useState, useEffect } from "react";
 import { useState, useEffect } from "react";
 import Feedback from "./components/Feedback/Feedback.jsx";
 import Options from "./components/Options/Options.jsx";
+import Notification from "./components/Notification/Notification.jsx";
+import Description from "./components/Description/Description.jsx";
 import s from "./App.module.css";
 
 const App = () => {
-  const [feedback, setFeedback] = useState(
-    JSON.parse(localStorage.getItem("feedback")) || {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    }
-  );
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
 
-  // Збереження стану у localStorage при кожній зміні
   useEffect(() => {
     localStorage.setItem("feedback", JSON.stringify(feedback));
   }, [feedback]);
 
-  const updateFeedback = (feedbackType) => {
-    setFeedback((prevState) => ({
-      ...prevState,
-      [feedbackType]: prevState[feedbackType] + 1,
+  const updateFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
     }));
   };
 
@@ -30,32 +29,29 @@ const App = () => {
   };
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedbackPercentage = totalFeedback
-    ? Math.round((feedback.good / totalFeedback) * 100)
-    : 0;
+  const positivePercentage =
+    totalFeedback > 0 ? ((feedback.good / totalFeedback) * 100).toFixed(0) : 0;
 
   return (
-    <div className={s.app}>
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
-
+    <div>
+      <Description />
       <Options
         updateFeedback={updateFeedback}
-        hasFeedback={totalFeedback > 0}
         resetFeedback={resetFeedback}
+        total={totalFeedback}
       />
 
       {totalFeedback > 0 ? (
-        <Feedback
-          feedback={feedback}
-          total={totalFeedback}
-          positivePercentage={positiveFeedbackPercentage}
-        />
+        <>
+          <Feedback
+            feedback={feedback}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+          <button onClick={resetFeedback}>Reset</button>
+        </>
       ) : (
-        <p>No feedback yet</p>
+        <Notification message="No feedback yet" />
       )}
     </div>
   );
